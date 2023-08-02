84699c1cfb2932eb102f9818cd5138af55a9089a
tomzwang11 <tomzwang11@gmail.com>
2023-08-02 05:22:05 +0000

    run eval

--- train.sh
+++ train.sh
@@ -1,5 +1,4 @@
-# export LD_PRELOAD=/home/liubingyu/.local/lib/python3.7/site-packages/skimage/_shared/../../scikit_image.libs/libgomp-d22c30c5.so.1.0.0
-# source /usr/local/Ascend/ascend-toolkit/set_env.sh 
+export LD_PRELOAD=/home/mindocr/.local/lib/python3.7/site-packages/skimage/_shared/../../scikit_image.libs/libgomp-d22c30c5.so.1.0.0
 
 export RANK_TABLE_FILE="hccl_4p_4567_127.0.0.1.json"
 export DEVICE_NUM=8
@@ -9,5 +8,5 @@ for ((i=0;i<$RANK_SIZE;i++));
 do
     export DEVICE_ID=$((i+4))
     export RANK_ID=$i
-    python train.py --config configs/yolov8/yolov8n.yaml --is_parallel True --device_per_servers 4 > run_train_$i.log 2>&1 &
+    python train.py --config configs/yolov8/yolov8n.yaml --is_parallel True --device_per_servers 4 --run_eval True > run_train_$i.log 2>&1 &
 done
